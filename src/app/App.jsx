afcb2c5da633fb4823723645410486a2c40955a2
MauricioH <rekedye@gmail.com>
2026-09-02 00:53:21 -0400

    Feat/16-implementar-autenticacion-y-creacion-de-cuentas-de-frontend-social-

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes.jsx'
+import { AuthProvider } from './providers/AuthProvider.jsx'
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
